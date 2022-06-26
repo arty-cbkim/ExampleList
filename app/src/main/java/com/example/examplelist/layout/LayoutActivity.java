5fb4770883884982e598c6f68c848bb08d15a1f1
cbkim <cbkim@artylab.co.kr>
2022-06-26 18:43:27 +0900

    layout test

---
+++ app/src/main/java/com/example/examplelist/layout/LayoutActivity.java
@@ -0,0 +1,15 @@
+package com.example.examplelist.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.examplelist.R;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+    }
+}
